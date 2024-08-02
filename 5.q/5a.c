71cd887af0b90cf00d895e38b4a087bc39886fbe
someOne-ai <anonymousinduty@gmail.com>
2024-08-02 14:38:21 +0530

    Update 5a.c

--- 5.q/5a.c
+++ 5.q/5a.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
-#include <math.h>
 
-// Function to calculate f(n)
-long f(int n) {
-    return 2 * pow(n, 3) + 3 * pow(n, 5);
+int f(int n) {
+    return 2 * n * n + 3 * n + 5;
+}
+
+int g(int n, int c) {
+    return c * n;
 }
 
 int main() {
-    int n0 = 1;
-    double c = 3.0; // c can be chosen to satisfy the condition
-
-    // Find n0 such that for all n >= n0, f(n) >= c * g(n)
-    while (f(n0) < c * pow(n0, 3)) {
-        n0++;
+    int n, c = 1;
+    printf("n\tf(n)\tc*g(n)\n");
+    for (n = 10; n <= 30; n++) {
+        printf("%d\t%d\t%d\t\n", n, f(n), g(n, c));
     }
-
-    printf("The value of n0 is: %d\n", n0);
-
-    // Plot the values of f(n) and c * g(n) for n from 10 to 30
-    printf("n\tf(n)\tc * g(n)\n");
-    for (int n = 10; n <= 30; n++) {
-        printf("%d\t%ld\t%.2f\n", n, f(n), c * pow(n, 3));
-    }
-
     return 0;
 }
