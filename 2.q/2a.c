2e59bd6066334cc34a1dcfb951806dc294044e40
someOne-ai <anonymousinduty@gmail.com>
2024-08-02 14:36:45 +0530

    Update 2a.c

--- 2.q/2a.c
+++ 2.q/2a.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
-#include <math.h>
 
-// Function to calculate f(n)
 int f(int n) {
     return 3 * n * n + 4 * n + 3;
 }
 
-int main() {
-    int n0 = 1;
-    double c = 1.0; // c can be a small constant
-
-    // Find n0 such that for all n >= n0, f(n) >= c * g(n)
-    while (f(n0) < c * n0) {
-        n0++;
-    }
-
-    printf("The value of n0 is: %d\n", n0);
-
-    // Plot the values of f(n) and c * g(n) for n from 10 to 30
-    printf("n\t f(n)\t c * g(n)\n");
-    for (int n = 10; n <= 30; n++) {
-        printf("%d\t%d\t%.2f\n", n, f(n), c * n);
-    }
-
-    return 0;
+int g(int n, int c) {
+    return c * n ;
 }
 
+int main() {
+    int n, c = 1;
+    printf("n\tf(n)\tc*g(n)\n");
+    for (n = 10; n <= 30; n++) {
+        printf("%d\t%d\t%d\t\n", n, f(n), g(n, c));
+    }
+    return 0;
+}
