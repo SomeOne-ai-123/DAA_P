40c3e7e5b872b14f132138c7e3f4a2efbce7042f
someOne-ai <anonymousinduty@gmail.com>
2024-08-07 21:58:53 +0530

    Update mergesort.c

--- 3.q/mergesort.c
+++ 3.q/mergesort.c
@@ -3,71 +3,34 @@
 #include <time.h>
 
 void merge(int arr[], int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+    int n1 = mid - left + 1, n2 = right - mid;
+    int L[n1], R[n2], i, j, k;
 
-    int L[n1], R[n2];
+    for (i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
 
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = left;
-
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+    for (i = 0, j = 0, k = left; k <= right; k++) {
+        if (j >= n2 || (i < n1 && L[i] <= R[j])) arr[k] = L[i++];
+        else arr[k] = R[j++];
     }
 }
 
 void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
-    }
+    if (left >= right) return;
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
 }
 
 int main() {
-    int n = 100000;
-    int arr[n];
-
+    int n = 100000, arr[n];
     srand(time(NULL));
-    for (int i = 0; i < n; i++) {
-        arr[i] = rand();
-    }
+    for (int i = 0; i < n; i++) arr[i] = rand();
 
     clock_t start = clock();
     mergeSort(arr, 0, n - 1);
-    clock_t end = clock();
-
-    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Merge Sort executed in %f seconds\n", time_taken);
+    printf("Merge Sort executed in %f seconds\n", (double)(clock() - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
